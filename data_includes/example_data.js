260c6c6ed16d7452293ffba99458aec8019b0b83
Daniel Blackey <dblackey@sas.upenn.edu>
2018-10-28 17:52:51 -0400

    h

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,8 +1,8 @@
 PennController.ResetPrefix(null);
-// Fetch the images there:
 // TODO: replace with wherever images end up
-PennController.AddHost("https://www.dropbox.com/home/LING300_dblackey_images/");
-// PennController.AddHost("http://files.lab.florianschwarz.net/ibexfiles/PennController/SampleTrials/");
+PennController.AddHost("http://files.lab.florianschwarz.net/ibexfiles/");
+
+// TODO: add consent form
 
 // INSTRUCTIONS
 PennController( "instructions" ,
@@ -15,10 +15,10 @@ PennController( "instructions" ,
     ,
     newText("intro3", "Place your fingers above the numeric keys 1, 2, 3 and 4 on your keyboard.")
     ,
-    newText("intro4", "Press 1, 2, 3 or 4 to start.")
+    newText("intro4", "Press the space bar to begin.")
     ,
-    newKey("numeric", "1234ï¿½") // Handle exception: an all-digit string would be evaluated as a charCode
-        .wait()                 // Start when 1, 2, 3 or 4 is pressed (weird character not on keyboard)
+    newKey("numeric", " ")
+        .wait()                 // start when space is pressed
 )
 .setOption("hideProgressBar", true); // Do not show the progress bar on first screen
 
@@ -27,50 +27,37 @@ PennController( "instructions" ,
 PennController.Template(  // Trials generated from design.csv from chunk_includes
   row => PennController( "trial" ,
     defaultImage
-        .settings.size(250, 250)            // Each image in this trial is 250 by 250
+        .settings.size(250, 250)
     ,
     defaultText
-        .settings.size(100, 25)             // Each text has same width as images (100px)
-        .settings.center()                  // Text labels will be centered below images
-    ,
-    newImage("image", row.picture + "?raw=1")
-    ,
-    newText("sentence", row.sentence)         // Retrieve the color name from the CSV design spreadsheet
-        .settings.css("font-size", "large") // Make it stand out
+        .settings.size(100, 25)
+        .settings.center()
         .print()
     ,
-    newKey("key", "TF")
+    newText("instruct", "Press T for true and F for false.")
+    ,
+    newImage("image", row.picture)
+    ,
+    newText("sentence", row.sentence)
+        .settings.css("font-size", "large")
+    ,
+    newKey("key", "tf")
+        .settings.log()  // log response
         .wait()
-    // newImage("image", row.picture + "?raw=1")
-  //   getSelector("patch")
-  //       .shuffle()                          // Shuffle the images (dummy Text element not part of selector yet)
-  //       .settings.disableClicks()           // Selection by key, not by click
-  //       .settings.keys( 49, 50, 51, 52 )    // Charcodes for keys 1, 2, 3 and 4 (all-digit strings would be evaluated as charCodes)
-  //       .settings.add( getText("null") )    // Add dummy Text element now (after shuffle and key assignment)
-  //       .settings.frame("solid 2px green")  // Positive visual feedback (green) by default
-  //       .settings.callback(
-  //           getSelector("patch").test.selected( getImage("color1") )
-  //               .success( getVar("score").set(v=>v+1) )                           // Increment score if color1 was selected
-  //               .failure( getSelector("patch").settings.frame("solid 2px red") )  // Red frame otherwise (negative visual feedback)
-  //       )
-  //       .settings.log()                     // Log which color was selected
-  //       .wait()                             // Wait for a selection (dummy Text automatically selected after 1s of no selection)
-  //       .settings.disable()                 // First selection is definitive
-  //   ,
-  //   newTimer("end", 500)                    // Wait 500ms before next trial (so you can see visual feedback frame)
-  //       .start()
-  //       .wait()
-  // )
-  // .log( "color", row.colorText )            // Log which color name was shown
   )
+  // log the label, sentence, and image file name
+  .log("label", row.label)
+  .log("sentence", row.sentence)
+  .log("image", row.picture)
 );
 
 
 // OLD IBEX SYNTAX FOR EARLY SENDING OF RESULTS
 var manualSendResults = true;
 var items = [["send", "__SendResults__", {}]];
-var shuffleSequence = seq("instructions", rshuffle("trial"), "send", "end"); // Order of labels reflects order of definition in this script
+var shuffleSequence = seq("instructions", rshuffle("trial"), "send", "end");
 
+// TODO: add debriefing
 
 // FINAL SCREEN
 PennController( "end" ,
@@ -80,4 +67,4 @@ PennController( "end" ,
     newTimer("ever", 1)                     // Dummy timer
         .wait()                             // Will wait forever (never started)
 )
-.setOption("countsForProgressBar", false); // No need to 'complete' this screen to fill the progress bar
+.setOption("countsForProgressBar", false);
