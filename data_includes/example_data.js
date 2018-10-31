4490e19defabf8743577918705e159a7489e59bf
Daniel Blackey <dblackey@sas.upenn.edu>
2018-10-31 19:19:30 -0400

    added consent form

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -4,18 +4,32 @@ PennController.ResetPrefix(null);
 
 // TODO: add consent form
 
+var consentForm = ["consent", "Form", {html: {include: "SONAconsentForm.html"}}];
+
+// TODO: add a line to instructions page that says make sure to get through the entire experiment (to the final page)
+// in order to receive credit for participation
+
+// TODO: have a friend take it timed
+
+PennController("consent",
+    newHtml("consentForm", "SONAconsentForm.html")
+        .print()
+)
+
 // INSTRUCTIONS
 PennController( "instructions" ,
     defaultText
         .print()
     ,
-    newText("intro1", "In this experiment, you will see a collection of picture-sentence pairs.")
+    newText("intro1", "In this experiment, you will see a collection of picture-sentence pairs. For each pair,")
     ,
-    newText("intro2", "For each pair, you will be asked to decide whether the sentence accurately describes the picture above it.")
+    newText("intro2", "you will be asked to decide whether the sentence accurately describes the picture above it.")
     ,
     newText("intro3", "You can press T for true and F for false. Try to make the best judgement possible.")
     ,
-    newText("intro4", "Press the space bar to begin.")
+    newText("intro4", "In order to receive credit for your participation, be sure not to exit until you are told")
+    ,
+    newText("intro5", "that the experiment has ended. Press the space bar to begin.")
     ,
     newKey("numeric", " ")
         .wait()                 // start when space is pressed
@@ -51,15 +65,19 @@ PennController.Template(  // Trials generated from design.csv from chunk_include
   .log("label", row.label)
   .log("sentence", row.sentence)
   .log("image", row.picture)
+  .log("ID", PennController.GetURLParameter("id"))
+  // TODO: see URL method for handling submissions tutorial
 );
 
 
 // OLD IBEX SYNTAX FOR EARLY SENDING OF RESULTS
 var manualSendResults = true;
 var items = [["send", "__SendResults__", {}]];
-var shuffleSequence = seq("instructions", rshuffle("trial"), "send", "end");
+var shuffleSequence = seq("consent", "instructions", rshuffle("trial"), "send", "end");
 
 // TODO: add debriefing
+
+// TODO: add comments
 
 // FINAL SCREEN
 PennController( "end" ,
